6f9e740814850e93e605e8ed7804b14967f5e283
Thoriq Putra Belligan <thoriqputrabelligan@mail.ugm.ac.id>
2025-06-02 11:42:02 +0700

    Update Robot_PID.ino

--- Arduino Programs/Robot_PID.ino
+++ Arduino Programs/Robot_PID.ino
@@ -1,16 +1,18 @@
-#define MOTOR_PIN1 11    // Motor A IN1 (Left)
-#define MOTOR_PIN2 10    // Motor A IN2 (Left)
-#define MOTOR_PIN3 6     // Motor B IN1 (Right)
-#define MOTOR_PIN4 5     // Motor B IN2 (Right)
+// mode PWM 70
+#define MOTOR_PIN1 6    // Motor A IN1 (Right)
+#define MOTOR_PIN2 5    // Motor A IN2 (Right)
+#define MOTOR_PIN3 10     // Motor B IN1 (Left)
+#define MOTOR_PIN4 11     // Motor B IN2 (Left)
 
 const int selectPins[] = {2, 3, 4}; 
 const int analogPin = A0;           
-const int threshold = 280;          
-const int SPEED_BASE = 180;         
+const int threshold = 250;          
+const int SPEED_BASE = 70;         
 
+const float scaler = 17;       
 const float Kp = 1.0;               
 const float Ki = 0.0001;               
-const float Kd = 0.001;              
+const float Kd = 0.01;              
 
 float previousError = 0;            
 float integral = 0;                 
@@ -26,6 +28,9 @@ void loop() {
   int sensorValues[8], weightedSum = 0, sum = 0;
   bool anySensorDetectsLine = false, allSensorsDetectLine = true;
 
+  // Declare the weights array ONCE, outside the loop, as float
+  float weights[8] = {-5, -3.3, -1.5, -0.5, 0.5, 1.5, 3.3, 5};  
+
   for (int i = 0; i < 8; i++) {
     selectChannel(i);
     int val = analogRead(analogPin);
@@ -33,16 +38,20 @@ void loop() {
     if (val < threshold) anySensorDetectsLine = true;
     else allSensorsDetectLine = false;
 
-    int weight = i - 3; 
+    float weight = weights[i];  // Use the float weight
     int sensorActive = (val < threshold) ? 1000 : 0;
     weightedSum += sensorActive * weight;
     sum += sensorActive;
+}
+
+  for (int i = 0; i < 8; i++) {
+  Serial.print(sensorValues[i]);
+  Serial.print("\t");
   }
 
-  for (int i = 0; i < 8; i++) { Serial.print(sensorValues[i]); Serial.print("\t"); }
-
   if (allSensorsDetectLine) {
-    stopMotors(); Serial.println("stop");
+  stopMotors();
+  Serial.println("stop");
   }
   else if (anySensorDetectsLine && sum != 0) {
     float error = (float)weightedSum / sum;
@@ -51,15 +60,23 @@ void loop() {
     float output = Kp * error + Ki * integral + Kd * derivative;
     previousError = error;
 
-    int adjustment = output * 40;
+    int adjustment = output * scaler;
     int leftSpeed = constrain(SPEED_BASE + adjustment, 0, 255);
     int rightSpeed = constrain(SPEED_BASE - adjustment, 0, 255);
 
     setMotorSpeeds(leftSpeed, rightSpeed);
-    Serial.print("PID: "); Serial.println(output);
+
+    Serial.print("PID: ");
+    Serial.print(output);
+    Serial.print("\tPWM Right: ");
+    Serial.print(leftSpeed);
+    Serial.print("\tPWM Left: ");
+    Serial.println(rightSpeed);
   }
   else {
-    delay(50); stopMotors(); Serial.println("stop");
+    delay(70);
+    stopMotors();
+    Serial.println("stop");
   }
 
   // delay(10);
